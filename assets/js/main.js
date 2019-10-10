2096d88af446d78af67c796d76d1494fcd044ea2
VEERAJ SHENOY <veeraj@3iology.com>
2019-10-10 08:04:43 +0530

    Projects added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
-$(document).ready(function(){
-    $('.fa-facebook-official').click(function(){
+$(document).ready(function () {
+    const projects_div = $('#projects');
+    $('.fa-facebook-official').click(function () {
         openLink('https://www.facebook.com/veeraj1991');
     });
 
@@ -18,10 +19,51 @@ $(document).ready(function(){
     $('.fa-stack-overflow').click(function () {
         openLink('https://stackexchange.com/users/5382849/veeraj-shenoy');
     });
+
+
+    let str = '';
+
+    projects.forEach(project => {
+        str += `
+    <div class="col-md-12 col-lg-6 mb-3">
+					<div class="card-height bg-light">
+						<div class="row no-gutters">
+							<div class="col-4 bg-white border d-none d-lg-block d-md-block">
+								<div class="height-100" style="background-image: url(assets/img/${project.img})">
+								</div>
+							</div>
+							<div class="col-md-8 col-lg-8 col-sm-12 col-xs-12">
+								<div class="card-body">
+                                    <h5 class="card-title">`;
+        if (project.url !== undefined && project.url.length !== 0) {
+            str += `<a href="${project.url[0]}" target='_blank'>${project.name}</a>`;
+        } else {
+            str += `${project.name}`;
+        }
+        str += `</h5>
+									<p class="card-text">${project.about}</p>
+									<p class="card-text"><small class="text-muted">Platforms - ${project.platform}</small>
+                                    </p>
+                                    <p>`;
+
+        project.technology.split(',').forEach(technology => {
+            str += `<span class="badge badge-primary">${technology}</span>&nbsp;`;
+        });
+
+        str += `</p>
+								</div>
+							</div>
+						</div>
+					</div>
+                </div>
+                `;
+    });
+
+    projects_div.html(str);
 });
 
 
-function openLink(url){
+function openLink(url) {
     var win = window.open(url, '_blank');
     win.focus();
 }
---
+++ assets/js/projects.js
@@ -0,0 +1,125 @@
+const projects = [{
+    name: '3ioNetra',
+    technology: 'Bootstrap, PHP, MySQL, jQuery',
+    platform: 'Desktop, Tablet',
+    about: '3ionetra is the perfect system for temples and Institutions to manage their income and expense tracking. The friendly user interface makes it quick and easy to capture your income and expense transactions for each day.',
+    img: '3ionetra.min.png',
+    url: ['http://www.3ionetra.in']
+}, {
+    name: 'Hariguru',
+    technology: 'Bootstrap, PHP, MySQL, jQuery',
+    platform: 'Desktop, Tablet',
+    about: 'Hariguru is an application specially designed for Hallbooking and Seva Booking of Dahisar Sri Kashi Math. It is used by the Math for seva & donation management.',
+    img: 'hariguru.min.png',
+    url: []
+}, {
+    name: 'Paypeoples',
+    technology: 'PHP, Bootstrap, MySQL, jQuery, Android SDK, Swift, AWS EC2, AWS S3 Bucket, AWS Face Rekognition',
+    platform: 'Desktop, Tablet',
+    about: 'Providing the best Payroll & hrms Software for every size business at an affordable price, with excellent customer support',
+    img: 'paypeoples.min.png',
+    url: ['http://www.paypeoples.com']
+}, {
+    name: '3ioFlash POS',
+    technology: 'Ionic PHP MySQL',
+    platform: 'Tablet',
+    about: 'Take your restaurant to a next level with 3ioFlash\'s sophisticated POS application, designed & powered with Machine Learning to enhance your customer\'s dining experience.',
+    img: '3iopos.min.png'
+}, {
+    name: 'Third wave power',
+    technology: 'Bootstrap, PHP',
+    platform: 'Desktop, Tablet, Mobile',
+    about: 'Third Wave Power aims to emPOWER people around the world, by creating portable power solutions that are essential for work and life, on or off the power grid. Third Wave Power’s renewable power solutions are used in field work, emergency back-up situations, outdoor and rural environments the world over, bringing POWER to people where it matters.',
+    img: 'thirdwavepower.min.jpg',
+    url: ['http://thirdwavepower.com']
+}, {
+    name: 'Dahisar Sri Kashi Math',
+    technology: 'PHP, MySQL, Bootstrap',
+    platform: 'Desktop, Tablet, Mobile',
+    about: 'Dahisar Sri Kashi Math is in Dahisar (E), the northen most western suburb of Greater Mumbai in Maharashtra.This project was conceived by Shrimad Sudhindra Tirtha Swami way back in eighties in the twentieth century on a plot of land at Dahisar(E) purchased in 1968 which is now popularly known as Sudhindranagar.',
+    img: 'dskm.min.png',
+    url: ['http://www.dskm.org']
+}, {
+    name: 'GSB Seva Mandal',
+    technology: 'Laravel, PHP, MySQL, jQuery',
+    platform: 'Desktop, Tablet, Mobile',
+    about: 'GSB Seva Mandal is a non profit organization registered in India under Bombay Public Trust Act, 1950 in the year 1951.',
+    img: 'gsbsevamandal.min.jpg',
+    url: ['http://gsbsevamandal.org']
+}, {
+    name: 'The GA Group',
+    technology: 'PHP, MYSQL, jQuery, Bootstrap',
+    platform: 'Desktop, Tablet, Mobile',
+    about: 'An international business consultancy facilitating Singapore and ASEAN companies to build business linkages, promoting cross border trade and investments in the APAC region.The company has a niche focus in organising high-level global diaspora business summits & conferences in the emerging economies of the Asia Pacific region.',
+    img: 'gagroup.min.jpg',
+    url: ['http://gagrp.com']
+}, {
+    name: 'Prithu Homes',
+    technology: 'PHP, MySQL, Bash Scripting',
+    platform: 'Desktop, Tablet, Mobile',
+    about: 'Pricing calculator & PDF template emailing for Prithu Homes',
+    img: 'prithu.min.png',
+    url: ['http://www.prithu.in']
+}, {
+    name: 'SIIEXPO',
+    technology: 'Bootstrap, PHP, MySQL',
+    platform: 'Desktop, Tablet, Mobile',
+    about: 'Singapore International Indian EXPO is the largest shopping and entertainment extravaganza in Singapore.',
+    img: 'siiexpo.min.jpg',
+    url: ['http://www.siiexpo.com']
+}, {
+    name: 'DEIDEAZ',
+    technology: 'Bootstrap, PHP, MySQL',
+    platform: 'Desktop, Tablet, Mobile',
+    about: 'Established in the year 2005, De Ideaz is a leading event management company offering full spectrum of business solutions from marketing to integrated tions.',
+    img: 'deideaz.min.jpg',
+    url: ['http://www.deideaz.com']
+}, {
+    name: 'GSB DB Navratri',
+    technology: 'PHP, MySQL, Ionic',
+    platform: 'Mobile',
+    about: 'Gaud Saraswat Brahmin Community, Dahisar-Borivali Branch\'s Seva collection app for GSBDB Navratri Utsav',
+    img: 'gsbdb.min.png',
+    url: ['https://play.google.com/store/apps/details?id=com.ionicframework.dbnav2286156']
+}, {
+    name: '3iosales',
+    technology: 'PHP, MySQL, Ionic',
+    platform: 'Desktop, Tablet, Mobile',
+    about: '3iosales is Customer Relationship Management software that enables the business to nurture relationships with its customers by aggregating streams of customer data.',
+    img: '3iosales.min.png',
+    url: [
+        'https://app.3iosales.com',
+        'https://play.google.com/store/apps/details?id=com.iosales.app'
+    ]
+}, {
+    name: 'Hello Punjabi',
+    technology: 'PHP, MySQL, Ionic',
+    platform: 'Tablet, Mobile',
+    about: 'Hello PUNJABI Mobile App - India\'s first social media app with sole purpose of bringing the people of entire punjabi community to a single platform.',
+    img: 'punjabi.min.png',
+    url: ['https://play.google.com/store/apps/details?id=com.punjabicommunity.app']
+}, {
+    name: 'Iris FMGS',
+    technology: 'Laravel, MySQL, Android SDK',
+    platform: 'Desktop, Mobile',
+    about: 'A facility management system that helps in achieving efficiency in the maintenance and operation process. It\'s highly customisable and configurable with user friendly design. It will have seamless work flow for modernising the maintenance day-to-day activities.',
+    img: 'iris.min.png',
+    url: [
+        'http://live.irisfmgs.com/',
+        'https://play.google.com/store/apps/details?id=com.management.facility'
+    ]
+}, {
+    name: 'Hospital Management System',
+    technology: 'PHP, MySQL, Bootstrap, jQuery',
+    platform: 'Desktop, Tablet',
+    about: 'A full fledged cash/cheque collection app. Also helps in monitoring and analysis of the patients and their medical history.',
+    img: 'hospital.min.png',
+    url: []
+}, {
+    name: 'Sagar Properties',
+    technology: 'PHP, MySQL, jQuery, HTML, CSS',
+    platform: 'Desktop, Tablet, Mobile',
+    about: 'Sagar Properties has been in the business of Real Estate Development for over three decades. With humble beginnings, establishment, and progress in the Mumbai real estate arena, we forayed into the Pune realty market in 2007-08.',
+    img: 'sagar-properties.min.png',
+    url: ['https://www.sagarproperties.com/']
+}];
