212a372d81aef9599fa270b125f83319e06c5272
VEERAJ SHENOY <veeraj@3iology.com>
2019-10-10 08:18:26 +0530

    side projects added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
     const projects_div = $('#projects');
+    const side_projects_div = $('#side_projects');
     $('.fa-facebook-official').click(function () {
         openLink('https://www.facebook.com/veeraj1991');
     });
@@ -60,6 +61,47 @@ $(document).ready(function () {
     });
 
     projects_div.html(str);
+
+
+    str = '';
+
+    side_projects.forEach(project => {
+        str += `
+    <div class="col-md-12 col-lg-6 mb-3">
+					<div class="card-height bg-light">
+						<div class="row no-gutters">
+							<div class="col-4 bg-white border d-none d-lg-block d-md-block">
+								<div class="height-100" style="background-image: url(assets/img/${project.img})">
+								</div>
+							</div>
+							<div class="col-md-8 col-lg-8 col-sm-12 col-xs-12">
+								<div class="card-body">
+                                    <h5 class="card-title">`;
+        if (project.url !== undefined && project.url.length !== 0) {
+            str += `<a href="${project.url[0]}" target='_blank'>${project.name}</a>`;
+        } else {
+            str += `${project.name}`;
+        }
+        str += `</h5>
+									<p class="card-text">${project.about}</p>
+									<p class="card-text"><small class="text-muted">Platforms - ${project.platform}</small>
+                                    </p>
+                                    <p>`;
+
+        project.technology.split(',').forEach(technology => {
+            str += `<span class="badge badge-primary">${technology}</span>&nbsp;`;
+        });
+
+        str += `</p>
+								</div>
+							</div>
+						</div>
+					</div>
+                </div>
+                `;
+    });
+
+    side_projects_div.html(str);
 });
 
 
---
+++ assets/js/side-projects.js
@@ -0,0 +1,70 @@
+const side_projects = [{
+    name: 'Animation app',
+    technology: 'Ionic',
+    platform: 'Mobile',
+    about: 'A simple app demonstrating implementation of animation css library in Ionic',
+    img: 'animation-app.min.png',
+    url: [
+        'https://play.google.com/store/apps/details?id=in.co.smartboy.animator',
+        'https://github.com/veerajongit/animator'
+    ]
+}, {
+    name: 'SmartBills 2.0 GST',
+    technology: 'PHP, MySQL, Bootstrap, jQuery',
+    platform: 'Desktop',
+    about: 'A sales management system for hard material companies. GST implementation to adhere to new Indian government compliance. Currently this project is deployed in Fine Contact and Precision Press Components located in Virar',
+    img: 'smartbills.min.png',
+    url: ['https://github.com/veerajongit/smartbills2.0-GST']
+}, {
+    name: 'PHP Blockchain',
+    technology: 'PHP',
+    platform: 'Server',
+    about: 'A simple Blockchain implemenetion done in PHP',
+    img: 'blockchain.min.jpg',
+    url: ['https://github.com/veerajongit/blockChain']
+}, {
+    name: 'Family Tree Maintainer',
+    technology: 'Bootstrap, jQuery, PHP, MySQL',
+    platform: 'Desktop, Tablet',
+    about: 'A simple family tree maintainer currently being used by GSB Seva Mandal Virar',
+    img: 'family-tree.jpg',
+    url: ['https://github.com/veerajongit/familytree']
+}, {
+    name: 'Shotokan Karate App',
+    technology: 'Ionic',
+    platform: 'Tablet, Mobile',
+    about: 'A mobile app developed showcasing Kata, Kumite and Karate demonstrations.',
+    img: 'karate.png',
+    url: ['https://github.com/veerajongit/shotokankarate']
+}, {
+    name: 'Tessaract Angular',
+    technology: 'JavaScript, Angular',
+    platform: 'Desktop, Tablet, Mobile',
+    about: 'Webapp example demonstration handwriting rekognition using Angular & Tessaract JS',
+    img: 'tesseract.jpg',
+    url: ['https://github.com/veerajongit/TessaractAngular']
+}, {
+    name: 'Image Loader Web Component',
+    technology: 'JavaScript, StencilJS',
+    platform: 'Desktop, Tablet, Mobile',
+    about: 'A simple webcomponent developed using StencilJS to lazy load & preload images in the DOM',
+    img: 'web-component.png',
+    url: [
+        'https://www.npmjs.com/package/@veerajshenoy/imageloader',
+        'https://github.com/veerajongit/image-loader'
+    ]
+}, {
+    name: 'Universal Software Center',
+    technology: 'Java, Bash, SSH, Ubuntu',
+    platform: 'Desktop',
+    about: 'A distributed software center that can load software\'s installed on server machine to local machines',
+    img: 'usc.min.jpg',
+    url: ['https://github.com/veerajongit/USC']
+}, {
+    name: 'SuperLoans',
+    technology: 'PHP, MySQL, Bootstrap,jQuery',
+    platform: 'Desktop, Table, Mobile',
+    about: 'At superloans we are helping people buy property seemlessly, helping you know Eligibility, Rates, process, necessary documents list, EMI comparison and transfer for lowest rates.',
+    img: 'superloans.min.png',
+    url: ['https://www.superloans.in/']
+}];
